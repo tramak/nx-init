f967c4da90007c181058fdceb03b59e028b19321
Viktor <kalaev-viktor@mail.ru>
2022-05-25 15:30:14 +0300

    Добавление презентационного компонента QuestionList

--- apps/ui-e2e/src/integration/header/header.spec.ts
+++ apps/ui-e2e/src/integration/header/header.spec.ts
@@ -1,5 +1,5 @@
 describe('ui: Todos component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=header'));
+  beforeEach(() => cy.visit('/iframe.html?id=common-header--logged-in'));
 
   it('should render the component', () => {
     cy.get('a').should('contain', 'Все активы');
--- libs/redux/src/lib/todos/sagas.ts
+++ libs/redux/src/lib/todos/sagas.ts
@@ -5,26 +5,6 @@ import * as actions from './actions';
 import registerRequest from '../../utils/registerRequest';
 
 
-// const fetchTodos = function* () {
-//   try {
-//     const [response]: [AxiosResponse<ITodos.Todo[]>] = yield all([call(api.fetchTodos)]);
-
-//     yield put(actions.setTodos(response.data));
-//   } catch(e) {
-//     console.log({ e });
-//   }
-//   yield 2;
-// }
-
-// const fetchAddTodo = function* (action: I.IFetchAddTodo) {
-//   try {
-//     const response: AxiosResponse<ITodos.Todo> = yield call(api.fetchAddTodo, action.payload);
-
-//     yield put(actions.addTodo(response.data));
-//   } catch (e) {
-//     console.log({ e });
-//   }
-// }
 
 const init = function* () {
   // yield takeEvery(actions.fetchTodos.type, fetchTodos);
